7bd477519be19b38d83865f036b3672c089638fd
shukry sharby <shukrysharby91@gmail.com>
2026-08-29 19:58:10 +0800

    Fix Astro Cloudflare config for current adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://homestayreview.my',
   output: 'server',
-  session: false,
   adapter: cloudflare({
     platformProxy: {
       enabled: true
