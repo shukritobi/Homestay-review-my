9b02d123d486b2b1d67a401597b1565f86e34a6b
shukry sharby <shukrysharby91@gmail.com>
2026-08-29 19:45:48 +0800

    Enable Cloudflare server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://homestayreview.my',
-  output: 'static',
+  output: 'server',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true
+    }
+  }),
   trailingSlash: 'never'
 });
