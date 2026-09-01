1e16477c608f1e4d5024838765c0d8273f81862d
shukry sharby <shukrysharby91@gmail.com>
2026-09-01 11:30:02 +0800

    Add more verbatim Timber Airbnb reviews

---
+++ src/data/rawPublicReviewsTimberMore.ts
@@ -0,0 +1,103 @@
+// Additional verbatim Timber Homestay Airbnb reviews supplied from the public review page.
+export const TIMBER_RAW_REVIEWS_MORE = [
+  {
+    author: 'Yvette', postedAt: '2026-07-01', dateLabel: 'July 2026', rating: 5,
+    text: 'It was a great relaxing stay! We spent most of our time in the pool.  The place is pretty well kept and cleanliness was superb. The aircons were working great and they have fans in all the rooms as well as the patio so it was nice and cool sitting outdoors even in the afternoons. kitchen comes fully equipped for cooking. Will be coming back and looking forward to another wonderful getaway!',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1473075693418931834'
+  },
+  {
+    author: 'Nuruljannah', postedAt: '2026-06-01', dateLabel: 'June 2026', rating: 5,
+    text: 'Me and family really love Bryce place!Kids love the pool the most!Thank u Bryce will definately come back here again!',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1476365969442484466'
+  },
+  {
+    author: 'Wendy', postedAt: '2026-06-01', dateLabel: 'June 2026', rating: 5,
+    text: 'We had a wonderful stay and would recommend this place for families with kids. The pool was a huge hit with the children and enjoyed the float provided. The house is located in a quiet and peaceful neighbourhood, yet it’s close to the Austin area, so there were plenty of convenient food delivery options if you do not want to drive out.\nThe house was spotless, exactly as shown in the photos, and the beds were very comfortable. We also appreciated that the entire home is on a single level, making it easy for elderly family members to move around safely. Overall, it was a comfortable and enjoyable stay, and we’d be happy to return.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1474539541146312588'
+  },
+  {
+    author: 'Jason', postedAt: '2026-06-01', dateLabel: 'June 2026', rating: 5,
+    text: 'Really enjoyed our stay here. The place was clean, cosy, and had everything we needed. The host was very helpful and easy to communicate with. Would happily stay here again!',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1474775821032148466'
+  },
+  {
+    author: 'Jon', postedAt: '2026-06-01', dateLabel: 'June 2026', rating: 5,
+    text: 'great host.very fast response.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1476107862983155087'
+  },
+  {
+    author: 'Churchill', postedAt: '2026-06-01', dateLabel: 'June 2026', rating: 5,
+    text: 'very homely',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1478514843136529935'
+  },
+  {
+    author: 'Sarah', postedAt: '2026-06-01', dateLabel: 'June 2026', rating: 5,
+    text: 'Overall, the photos matched the actual accommodation. The host was very responsive and helpful throughout our stay. Whenever we encountered any issues, the host was available to assist us promptly.\nThe children especially enjoyed the pool and had a great time during our stay. Overall, we had a good experience and enjoyed our stay. Thank you.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1475076302428093537'
+  },
+  {
+    author: 'Thivyaa', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: 'Very beautiful house and worth the stay . We really enjoyed the karaoke at living room . Will come again .',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1473954196812414517'
+  },
+  {
+    author: 'Shaik', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: 'lovely place to stay with friends or family. the whole vibe was very cozy.. we enjoyed it alot.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1495888730384054580'
+  },
+  {
+    author: 'Wenleen', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: 'We had a great stay from start to finish. The host was organised, welcoming, and made the whole experience easy. Everything matched the listing, and we would happily stay again.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1489836010943855988'
+  },
+  {
+    author: 'Mohammad Hanizam', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: 'Best stay ever. Thanks a lot...',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1497078187688980797'
+  },
+  {
+    author: 'Raj', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: "it was actually a perfect place for our team away day, and I'll be.making a reservation again for a staycation with friends",
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1472769511719220232'
+  },
+  {
+    author: 'Chng', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: 'Good place clean and tidy when arrived. Check in and out was simple',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1484926891339690136'
+  },
+  {
+    author: 'R R', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: 'A wonderful experience overall. The host was responsive and attentive, always ready to help. The apartment was clean, comfortable, and cosy, and most importantly, it looked exactly like the pictures....no surprises! Would gladly return again.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1477299395788054535'
+  },
+  {
+    author: 'Izwan', postedAt: '2026-05-01', dateLabel: 'May 2026', rating: 5,
+    text: 'Nice homestay, kids love it.  Will definately return and the host is responsive and understanding.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1674536595265470435'
+  },
+  {
+    author: 'Wai Hang', postedAt: '2026-04-01', dateLabel: 'April 2026', rating: 5,
+    text: 'The house is in a beautiful setting and is also very spacious. Every little detail is well taken care of.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1492232331241290609'
+  },
+  {
+    author: 'Arfaan', postedAt: '2026-04-01', dateLabel: 'April 2026', rating: 5,
+    text: "it was a really bang for ur buck place and all the amenities were super nice, there's everything that u cld ask for, and honestly the area it was located in was quite interesting to explore too despite it being abit further away from touristy areas, overall rlly enjoyed my stay there",
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1504294060659903134'
+  },
+  {
+    author: 'Heng Yu', postedAt: '2026-04-01', dateLabel: 'April 2026', rating: 5,
+    text: "This was one of the best homes I've stayed at in Johor and was clean throughout the whole house and very worth the money will stay again \\:D",
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1488301376523919610'
+  },
+  {
+    author: 'முதல்வன் இளம்பாரி', postedAt: '2026-04-01', dateLabel: 'April 2026', rating: 5,
+    text: 'Great place to chill n relax surrounding good shops food eatries',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1486274543935294312'
+  },
+  {
+    author: 'Zevin', postedAt: '2026-04-01', dateLabel: 'April 2026', rating: 5,
+    text: 'This place is amazing. The bonus of this place is the pool. Kids will definitely enjoy playing in the pool everyday. Had a great time staying here and hosts are extremely responsive and helpful. Highly recommend for families with kids to stay here.',
+    sourceUrl: 'https://www.airbnb.com/rooms/627430285062581362/reviews', sourceLabel: 'Airbnb', authorUrl: 'https://www.airbnb.com/users/profile/1482193017664212703'
+  }
+];
